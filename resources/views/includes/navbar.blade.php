057320f685bfb6cc62705d033557f534c4f8c711
aniruddha iyer <aniruddha38@outlook.com>
2018-03-12 15:50:29 +0530

    suggestion page. almost done.

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
       <div class="col-12 menu-bar">
-            <a title="alterbit-home" href="{{URL::to('/')}}">alterBiT<span> | The Unconventional Way of Life</span></a>
+            <a href="{{URL::to('/')}}">alterBIT<span> | The Unconventional Way of Life</span></a>
       </div>
   </div>
